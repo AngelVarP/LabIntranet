b9c430da9e8f1879ca60cf1479692f70d940e02b
Rafael <adriano.olivos.g@uni.pe>
2025-09-24 13:14:32 -0500

    prueba en rama

---
+++ views/usuario/index.php
@@ -0,0 +1,26 @@
+<?php
+    
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>LabIntranet</title>
+    <link rel="stylesheet" href="/assets/css/usuario.css">
+</head>
+<body>
+
+    <header>
+        <div class="logo">
+            <img src="icono.png" alt="Logo" class="icono">
+            <span>LabIntranet</span>
+        </div>
+        <div class="acciones">
+            <button>Iniciar Sesión</button>
+            <button>Registrarse</button>
+        </div>
+    </header>
+
+    
+</body>
+</html>
