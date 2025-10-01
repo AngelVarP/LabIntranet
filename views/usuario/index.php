7bfe92877ea104ed82b707316f46e0de11e5df07
Rafael <adriano.olivos.g@uni.pe>
2025-10-01 01:21:51 -0500

    header

--- views/usuario/index.php
+++ views/usuario/index.php
@@ -7,17 +7,28 @@
     <meta charset="UTF-8">
     <title>LabIntranet</title>
     <link rel="stylesheet" href="/assets/css/usuario.css">
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
 
     <header>
-        <div class="logo">
-            <img src="assets/images/icono.png" alt="Logo" class="icono">
-            <span>LabIntranet</span>
-        </div>
-        <div class="acciones">
-            <button>Iniciar Sesión</button>
-            <button>Registrarse</button>
+        <div class="container">
+
+
+            <div class="logo-wrapper">
+
+                <div class="logo-box">
+                    <img src="/assets/images/logo.png" alt="Logo" class="icono">
+                </div>
+
+                <span class="logo-text">LabIntranet</span>
+            </div>
+
+
+            <div class="acciones">
+                <button class="btn-iniciar">Iniciar Sesión</button>
+                <button class="btn-registrar">Registrarse</button>
+            </div>
         </div>
     </header>
 
