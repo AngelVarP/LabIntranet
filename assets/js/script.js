ebe9af3844b1928a9b87de847fecf52e2b78c506
DinZ-24 <dino.suazo.z@uni.pe>
2025-10-02 23:17:24 -0500

    Listo

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,28 +6,28 @@ loginForm.addEventListener('submit', function(event) {
     event.preventDefault(); // Previene el comportamiento por defecto del formulario (recargar página)
 
     // Obtener datos del formulario
-    const username = document.getElementById('username').value;
+    const username = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const role = document.getElementById('role').value;
 
     // Validar que los campos no estén vacíos
-    if (username === "" || password === "" || role === "") {
+    if (email === "" || password === "" || role === "") {
         alert("Por favor, completa todos los campos.");
         return; // Detiene la ejecución si los campos están vacíos
     }
 
     // Guardar en localStorage
     // Nota: Deberías validar credenciales reales en un servidor antes de guardar.
-    localStorage.setItem("usuario", username);
+    localStorage.setItem("email", email);
     localStorage.setItem("rol", role);
 
     // Redirigir según el rol
     if (role === "delegado") { // Asegúrate que el valor sea 'Delegado' (mayúscula/minúscula)
-        window.location.href = "../../views/delegado/delegado.html";
+        window.location.href = "/views/delegado/delegado.html";
         alert("¡Bienvenido Delegado! Accediendo a la página del delegado.");
     } else if (role === "instructor") { // Usamos 'else if' para otra opción
         // La ruta debe ser a la página principal del Instructor
-        window.location.href = "../../views/instructor/solicitudes_instructor.html";
+        window.location.href = "/views/instructor/instructor.html";
         alert("¡Bienvenido Instructor! Accediendo a la página del instructor.");
     } else {
         alert("Rol no soportado para esta demo.");
